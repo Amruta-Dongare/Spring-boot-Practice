8a29112dd8e967c660001fb30853cfb32e288dd7
Amruta-Dongare <amrutadongare2001@gmail.com>
2026-03-25 21:39:52 +0530

    Normal Spring boot project created  to learn about the Spring boot

---
+++ HelloSpringBoot/HelloSpringBoot/src/main/java/com/example/demo/bean/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.demo.bean;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld {
+    public String sayHelloWorld(){
+        return "In the sayHelloWorld Method";
+    }
+}
